27406d9ac78de5a8124a0c900c187f3f7bcc09a6
Dang Hoang Viet <86547972+HoangViet208@users.noreply.github.com>
2024-12-08 01:42:12 +0700

    Update new BE URL for TimeSheet.js

--- src/Page/Manager/TimeSheet/TimeSheet.js
+++ src/Page/Manager/TimeSheet/TimeSheet.js
@@ -164,7 +164,7 @@ export default function TimeSheet() {
         const { format } = require('date-fns')
         try {
             const downloadResponse = await axios.get(
-                `https://timekeepingsystem.somee.com/api/WorkSlotEmployee/export-excel-file?departmentId=${
+                `https://timekeepingsystem.runasp.net/api/WorkSlotEmployee/export-excel-file?departmentId=${
                     Department == 'AllTeam' ? `00000000-0000-0000-0000-000000000000` : Department
                 }&month=${format(selectedDateRange.startDate, 'yyyy/MM/dd')}`,
                 {
